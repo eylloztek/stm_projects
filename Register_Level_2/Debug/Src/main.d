a7ab23298666fdb83172b40378aa3fd8976a53d0
eylloztek <eyluloztek2@gmail.com>
2025-07-02 23:21:13 +0300

    register level 2 - turn on led with user button

---
+++ Register_Level_2/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Include/core_cm4.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_version.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_compiler.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_gcc.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Include/mpu_armv7.h \
+ D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Include/core_cm4.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_version.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_compiler.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Include/cmsis_gcc.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Include/mpu_armv7.h:
+D:/stmworkspace/Register_Level_2/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
